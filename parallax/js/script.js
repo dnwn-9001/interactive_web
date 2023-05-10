b5825bb74e72fadf96ed8bf0db951defe88c0f38
dnwn-9001 <j10511@hanmail.net>
2023-05-10 17:49:06 +0900

    mousemove 추가

--- parallax/js/script.js
+++ parallax/js/script.js
@@ -1,9 +1,11 @@
 let scrollNum = 0;
-let documentHeight = 0;
-let windowHeight = 0;
+let pageX = 0;
+let targetX = 0;
+const speed = 0.1;
 
 const imageAll = document.querySelectorAll(".imageWrap .parallaxImage");
 const totalNum = imageAll.length;
+const subPageImage = document.querySelector(".subPage .parallaxImage");
 
 window.addEventListener("scroll", () => {
   scrollNum = window.scrollY;
@@ -13,3 +15,21 @@ window.addEventListener("scroll", () => {
     }px)`;
   });
 });
+
+window.addEventListener("mousemove", (e) => {
+  pageX = e.pageX - window.innerWidth / 2;
+});
+
+const loop = () => {
+  targetX += (pageX - targetX) * speed;
+  imageAll[4].style.transform = `scale(1.05) translate(${-targetX / 100}px,${
+    -scrollNum / (2 * (totalNum - 4))
+  }px)`;
+  imageAll[5].style.transform = `scale(1.05) translate(${-targetX / 100}px,${
+    -scrollNum / (2 * (totalNum - 5))
+  }px )`;
+  subPageImage.style.transform = `scale(1.1) translateX(${-targetX / 50}px)`;
+  window.requestAnimationFrame(loop);
+};
+
+loop();
